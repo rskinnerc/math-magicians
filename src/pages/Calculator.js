bd406c6b42f9ef5f4cc04de662bfd75d1cfa663a
Ronald <rjskinnerc@unal.edu.co>
2022-05-31 14:54:32 -0500

    Style the calculator page

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -1,9 +1,9 @@
 import CalculatorWidget from '../components/Calculator';
 
 const Calculator = () => (
-  <div>
-    <h1>Let&apos;s do some math!</h1>
-    <section className="calculatorContainer">
+  <div className="p-5">
+    <h1 className="text-white text-2xl font-semibold my-5">Let&apos;s do some math!</h1>
+    <section className="text-white font-bold">
       <CalculatorWidget />
     </section>
   </div>
