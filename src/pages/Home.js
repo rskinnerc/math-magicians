f9b5c965f7a2acdc6ec9371a8bdef788905de2e7
Ronald <rjskinnerc@unal.edu.co>
2022-05-31 14:50:17 -0500

    Style the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 const Home = () => (
-  <div>
-    <h1>Welcome to our page!</h1>
-    <p>
+  <div className="p-5">
+    <h1 className="text-white text-2xl font-semibold my-5">Welcome to our page!</h1>
+    <p className="text-sky-100 mb-5">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Massa id neque aliquam vestibulum.
       Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor.
@@ -11,7 +11,7 @@ const Home = () => (
       sit amet mattis. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim.
       In est ante in nibh mauris. Arcu cursus euismod quis viverra.
     </p>
-    <p>
+    <p className="text-sky-100 mb-5">
       Varius vel pharetra vel turpis nunc eget lorem dolor. Posuere ac ut consequat
       semper viverra nam libero justo. Quam id leo in vitae turpis massa. Risus feugiat
       in ante metus dictum at tempor commodo ullamcorper. Aliquet risus feugiat in ante
