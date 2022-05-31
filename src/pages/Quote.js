5fbaee6267a2b588f0bcb8873a4e82a7ac8929a5
Ronald <rjskinnerc@unal.edu.co>
2022-05-31 15:07:00 -0500

    Style the quotes page

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,7 +1,11 @@
 const Quote = () => (
-  <div>
-    <h1>Quote Page</h1>
-    <p>Some Random Quote</p>
+  <div className="p-5">
+
+    <p className="text-sky-100 font-semibold italic text-2xl mt-24 text-right">
+      &ldquo;Without mathematics, there’s nothing you can do. Everything around you is
+      mathematics. Everything around you is numbers.&rdquo;
+    </p>
+    <p className="text-zinc-400 text-lg text-right italic my-5">— Shakuntala Devi, Indian writer and mental calculator</p>
   </div>
 );
 
