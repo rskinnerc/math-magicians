34efbf2513f52cd1b29455d0687b992883b9e668
Ronald <rjskinnerc@unal.edu.co>
2022-05-31 11:55:06 -0500

    Add Home route and page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './app.css';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 // eslint-disable-next-line react/prefer-stateless-function
 const App = () => (
   <div className="App">
+    <Navbar />
     <Routes>
       <Route index element={<Home />} />
     </Routes>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 const Home = () => (
   <div>
-    Home Page
-    <div>Content</div>
+    <h1>Welcome to our page!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Massa id neque aliquam vestibulum.
+      Imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor.
+      Mi bibendum neque egestas congue quisque egestas diam in.
+      Tellus molestie nunc non blandit massa enim. Sed turpis tincidunt id aliquet
+      risus. Netus et malesuada fames ac turpis. Libero nunc consequat interdum varius
+      sit amet mattis. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim.
+      In est ante in nibh mauris. Arcu cursus euismod quis viverra.
+    </p>
+    <p>
+      Varius vel pharetra vel turpis nunc eget lorem dolor. Posuere ac ut consequat
+      semper viverra nam libero justo. Quam id leo in vitae turpis massa. Risus feugiat
+      in ante metus dictum at tempor commodo ullamcorper. Aliquet risus feugiat in ante
+      metus dictum at. Lectus nulla at volutpat diam ut venenatis tellus.
+      Phasellus egestas tellus rutrum tellus.
+    </p>
   </div>
 );
 
