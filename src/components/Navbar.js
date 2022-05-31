cbf8b29e5587a3089eb2d53d763d4202640dfe44
Ronald <rjskinnerc@unal.edu.co>
2022-05-31 15:10:41 -0500

    Add inline styles to the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
 
   return (
     <nav>
-      <h1 className="text-rose-500 text-3xl text-center py-10 font-bold">Math Magicians</h1>
+      <h1 className="text-rose-500 text-3xl text-center py-10 font-bold" style={{ textShadow: '1px 1px 1px rgba(206,206,206,0.7)' }}>Math Magicians</h1>
       <ul className="flex justify-around">
         <li><Link to="/" className={`${activeClass('/')} hover:text-rose-400 font-semibold p-2`}>Home</Link></li>
         <li><Link to="/calculator" className={`${activeClass('/calculator')} hover:text-rose-400 font-semibold p-2`}>Calculator</Link></li>
